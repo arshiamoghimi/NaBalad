306512ad2e417a99df27ac90e1caead77b2a1b32
arshia Moghimi <arshiamoghimi98@gmail.com>
2021-05-03 14:46:56 +0430

    search implemented, still buggy

--- app/src/main/java/ir/sambal/nabalad/MapsFragment.kt
+++ app/src/main/java/ir/sambal/nabalad/MapsFragment.kt
@@ -1,19 +1,44 @@
 package ir.sambal.nabalad
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.droidnet.DroidNet
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.gson.JsonObject
+import com.mapbox.api.geocoding.v5.models.CarmenFeature
+import com.mapbox.geojson.Feature
+import com.mapbox.geojson.FeatureCollection
+import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
 
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(), OnMapReadyCallback {
+    private var mapboxMap: MapboxMap? = null
+    private val REQUEST_CODE_AUTOCOMPLETE = 1
     private var mapView: MapView? = null
+    private val geojsonSourceLayerId = "geojsonSourceLayerId"
+    private val symbolIconId = "symbolIconId"
+    private var home: CarmenFeature? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,10 +60,94 @@ class MapsFragment : Fragment() {
 
         mapView = view.findViewById(R.id.mapView)
         mapView?.onCreate(savedInstanceState)
-        mapView?.getMapAsync { mapboxMap ->
-            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+        mapView?.getMapAsync(this)
+    }
 
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        this.mapboxMap = mapboxMap
+        mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+            initSearchFab()
+
+            addUserLocations()
+
+            setUpSource(it)
+
+            setUpLayer(it)
+        }
+    }
+
+    private fun addUserLocations() {
+        home = CarmenFeature.builder().text("Mapbox SF Office")
+            .geometry(Point.fromLngLat(-122.3964485, 37.7912561))
+            .placeName("50 Beale St, San Francisco, CA")
+            .id("mapbox-sf")
+            .properties(JsonObject())
+            .build()
+    }
+
+    private fun setUpLayer(style: Style) {
+        style.addLayer(
+            SymbolLayer("SYMBOL_LAYER_ID", geojsonSourceLayerId).withProperties(
+                iconImage(symbolIconId),
+                iconOffset(arrayOf(0f, -8f))
+            )
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_AUTOCOMPLETE) {
+            val selectedCarmenFeature = PlaceAutocomplete.getPlace(data)
+
+            if (mapboxMap != null) {
+                val style = mapboxMap!!.style
+                if (style != null) {
+                    val source = style.getSourceAs<GeoJsonSource>(geojsonSourceLayerId)
+                    source?.setGeoJson(
+                        FeatureCollection.fromFeatures(
+                            arrayOf(
+                                Feature.fromJson(
+                                    selectedCarmenFeature.toJson()
+                                )
+                            )
+                        )
+                    )
+                    mapboxMap!!.animateCamera(
+                        CameraUpdateFactory.newCameraPosition(
+                            CameraPosition.Builder()
+                                .target(
+                                    LatLng(
+                                        (selectedCarmenFeature.geometry() as Point?)!!.latitude(),
+                                        (selectedCarmenFeature.geometry() as Point?)!!.longitude()
+                                    )
+                                )
+                                .zoom(14.0)
+                                .build()
+                        ), 4000
+                    )
+                }
             }
+        }
+    }
+
+    private fun setUpSource(style: Style) {
+        style.addSource(GeoJsonSource(geojsonSourceLayerId))
+    }
+
+    fun initSearchFab() {
+        view?.findViewById<FloatingActionButton>(R.id.fab_location_search)?.setOnClickListener {
+            val intent = PlaceAutocomplete.IntentBuilder()
+                .accessToken(Mapbox.getAccessToken()!!)
+                .placeOptions(
+                    PlaceOptions.builder()
+                        .backgroundColor(Color.parseColor("#EEEEEE"))
+                        .limit(10)
+                        .addInjectedFeature(home)
+                        .build(PlaceOptions.MODE_CARDS)
+                )
+                .build(activity)
+            startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE)
         }
     }
 
